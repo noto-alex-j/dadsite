c3e8b6e57d1d65453b35de5c2bd208cf7350476e
Alexander Noto <noto.alex.j@gmail.com>
2017-10-06 22:58:36 -0500

    changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   validates :name, :unit_quantity, :unit_cost, presence: true
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   validates :name, :address, :phone, presence: true
+  has_many :order_items
+  has_many :items, through: :order_items
 end
